5a73b08c9cc68f3e008207534302f5ce69de8e5e
ghofur <algar0940@gmail.com>
2025-06-20 16:55:50 +0700

    ubah halaman home

--- src/resources/content.js
+++ src/resources/content.js
@@ -166,18 +166,18 @@ const about = {
         description: <>I make sensors talk to the cloud—sometimes better than humans do.</>,
         // optional: leave the array empty if you don't want to display images
         images: [
-          {
-            src: "/images/projects/project-01/cover-02.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-          {
-            src: "/images/projects/project-01/cover-03.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
+          // {
+          //   src: "/images/projects/project-01/cover-02.jpg",
+          //   alt: "Project image",
+          //   width: 16,
+          //   height: 9,
+          // },
+          // {
+          //   src: "/images/projects/project-01/cover-03.jpg",
+          //   alt: "Project image",
+          //   width: 16,
+          //   height: 9,
+          // },
         ],
       },
       {
